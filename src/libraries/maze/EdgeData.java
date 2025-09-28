a08a195255ebe8db0488d8415a53c3ddd43f4561
samuel <samueldeguio@gmail.com>
2025-09-28 19:21:25 +0200

    feat: add maze specific additional data for graph

---
+++ src/libraries/maze/EdgeData.java
@@ -0,0 +1,16 @@
+package libraries.maze;
+
+public class EdgeData {
+
+    boolean wall = false;
+
+    // GETTERS / SETTERS ------------------------------------------------------
+
+    public boolean isWall() {
+        return wall;
+    }
+
+    public void setWall(boolean wall) {
+        this.wall = wall;
+    }
+}
---
+++ src/libraries/maze/NodeData.java
@@ -0,0 +1,16 @@
+package libraries.maze;
+
+public class NodeData {
+
+    boolean visited = false;
+
+    // GETTERS / SETTERS ------------------------------------------------------
+
+    public boolean isVisited() {
+        return visited;
+    }
+
+    public void setVisited(boolean visited) {
+        this.visited = visited;
+    }
+}
