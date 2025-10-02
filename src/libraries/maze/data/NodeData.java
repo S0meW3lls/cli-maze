d6a5110fb8374cbd776863327cefad44d18dc96a
samuel <samueldeguio@gmail.com>
2025-10-02 22:48:32 +0200

    feat: add coordinates to node data

--- src/libraries/maze/data/NodeData.java
+++ src/libraries/maze/data/NodeData.java
@@ -2,7 +2,20 @@ package libraries.maze.data;
 
 public class NodeData {
 
-    boolean visited = false;
+    private boolean visited;
+    private int x;
+    private int y;
+
+    public NodeData(int x, int y) {
+
+        // set node position
+        this.x = x;
+        this.y = y;
+
+        // set default data
+        this.visited = false;
+
+    }
 
     // GETTERS / SETTERS ------------------------------------------------------
 
@@ -12,5 +25,21 @@ public class NodeData {
 
     public void setVisited(boolean visited) {
         this.visited = visited;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
     }
 }
